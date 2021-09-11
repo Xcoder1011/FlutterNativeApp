b842c55ed884e5fd283ff9ec7db30eae283897d1
wushangkun <wushangkun@chelun.com>
2021-09-11 12:11:23 +0800

    add initial demo

---
+++ BasicAppSwift/BasicAppSwift/ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  BasicAppSwift
+//
+//  Created by shangkun on 2021/9/10.
+//
+
+import UIKit
+import Flutter
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        self.view.backgroundColor = UIColor.red
+        
+        let button = UIButton(type: .system)
+        button.setTitle("跳转", for: .normal)
+        button.frame = CGRect(x:10, y:150, width:100, height:30)
+        button.addTarget(self, action:#selector(self.handleButtonAction), for:.touchUpInside)
+        self.view.addSubview(button)
+    }
+    
+    
+    @objc func handleButtonAction() {
+        let flutterEngine = (UIApplication.shared.delegate as? AppDelegate)?.flutterEngine;
+        let flutterViewController = FlutterViewController(engine: flutterEngine!, nibName: nil, bundle: nil);
+        self.present(flutterViewController, animated: true, completion: nil)
+      }
+
+}
+
---
+++ BasicAppSwift/Pods/Target Support Files/Pods-BasicAppSwift/Pods-BasicAppSwift-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_BasicAppSwift : NSObject
+@end
+@implementation PodsDummy_Pods_BasicAppSwift
+@end
